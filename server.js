068bf55852cd92ad00d69ea58264dce5f9d46501
Mathew Robinson <mathew@chasinglogic.io>
2025-09-29 06:38:57 +0100

    feat: optionally respond with the headers as JSON

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const port = process.env.PORT || 8000;
 const statusCode = process.env.STATUS_CODE || 200;
+const respondWithHeaders =
+  (process.env.RESPOND_WITH_HEADERS || "true") === "true";
 
 function handler(req, res) {
   const isHealthCheck = req.url.startsWith("/health");
@@ -8,13 +10,23 @@ function handler(req, res) {
 
   console.log("Request:", req.method, req.url);
 
-  console.log("Headers:");
-  console.group();
-  console.log(req.headers);
-  console.groupEnd();
+  const info = {
+    request: {
+      method: req.method,
+      url: req.url,
+    },
+    headers: req.headers,
+  };
 
+  console.log(info);
   console.log("=================");
-  res.end("");
+
+  if (respondWithHeaders) {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(info));
+  } else {
+    res.end("done");
+  }
 }
 
 http.createServer(handler).listen(port, () => {
